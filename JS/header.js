1218ccb03bc7e35a8692f50e825f64776a78d261
Matías Jordán <matiasowjordan@gmail.com>
2023-08-07 14:20:37 -0300

    tarjetas terminadas, agrego footer y boton de like

--- JS/header.js
+++ JS/header.js
@@ -1,37 +1,35 @@
 const nav = document.querySelector('.navbar');
 const abrir = document.getElementById('abrir');
 const cerrar = document.getElementById('cerrar');
-const imagen = document.getElementById('imagen');
 const botonGeneros = document.getElementById('boton-generos');
 const botonDecadas = document.getElementById('boton-decadas');
 const dropdownGeneros = document.querySelector('.dropdown-generos')
 const dropdownDecadas = document.querySelector('.dropdown-decadas')
+const decadas = document.querySelector('.contenedorDecadas');
+const generos = document.querySelector('.contenedorGeneros');
 
 
 abrir.addEventListener('click', () => {
 	nav.classList.add("visible");
     cerrar.style.display = 'block';
-    imagen.style.display = 'none';
-})
+});
 
 cerrar.addEventListener('click', ()=> {
     nav.classList.remove('visible');
-    imagen.style.display = 'flex';
-})
+    cerrar.style.display = 'none';
+});
 
 botonGeneros.addEventListener('click', () => {
-    const caret = botonGeneros.querySelector('.fa-solid.fa-caret-up');
-    dropdownGeneros.style.opacity = dropdownGeneros.style.opacity === '1' ? '0' : '1';
-    dropdownGeneros.style.visibility = dropdownGeneros.style.opacity === '1' ? 'visible' : 'hidden';
+    const caret = generos.querySelector('.fa-solid.fa-caret-up');
+    dropdownGeneros.style.display = dropdownGeneros.style.display === 'block' ? 'none' : 'block';
     caret.classList.toggle('fa-caret-down');
 });
 
 botonDecadas.addEventListener('click', () => {
-    const caret = botonDecadas.querySelector('.fa-solid.fa-caret-up');
-    dropdownDecadas.style.opacity = dropdownDecadas.style.opacity === '1' ? '0': '1';
-    dropdownDecadas.style.visibility = dropdownDecadas.style.opacity === '1' ? 'visible' : 'hidden';
+    const caret = decadas.querySelector('.fa-solid.fa-caret-up');
+    console.log(caret);
+    dropdownDecadas.style.display = dropdownDecadas.style.display === 'block' ? 'none' : 'block';
     caret.classList.toggle('fa-caret-down');
-    
 });
 
 
